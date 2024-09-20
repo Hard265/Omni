e67113920cafa1c28e621f8265ee421d8d9d08aa
Hard265 <hardynamakhwa@gmail.com>
2024-09-20 10:48:31 +0000

    update

--- src/components/HomeAppBar.tsx
+++ src/components/HomeAppBar.tsx
@@ -1,11 +1,12 @@
-import {Animated} from 'react-native';
+import { useTheme } from '@react-navigation/native';
+import { Animated } from 'react-native';
 
 interface HomeAppBarProps {
-  translateY: number
+  translateY: number;
 }
 
-export default function HomeAppBar({ translateY }: HomeAppBarProps ) {
-  const { colors } =  useTheme();
+export default function HomeAppBar({ translateY }: HomeAppBarProps) {
+  const { colors } = useTheme();
 
   return (
     <Animated.View
@@ -15,14 +16,14 @@ export default function HomeAppBar({ translateY }: HomeAppBarProps ) {
         backgroundColor: colors.card,
         width: '100%',
         height: 64,
-        transform: [{translateY: translateY}],
+        transform: [{ translateY: translateY }],
         position: 'absolute',
         top: 0,
         right: 0,
         left: 0,
         elevation: 4,
         zIndex: 1,
-}}>
-   </Animated.View>
+      }}
+    ></Animated.View>
   );
-};
+}
