82dad5c58c0f799c3fd2e5a7f59f4f4790efdf2a
Hard265 <hardynamakhwa@gmail.com>
2024-09-20 09:59:43 +0000

    add a header component

---
+++ src/HomeAppBar.tsx
@@ -0,0 +1,28 @@
+import {Animated} from 'react-native';
+
+interface HomeAppBarProps {
+  translateY: number
+}
+
+export default function HomeAppBar({ translateY }: HomeAppBarProps ) {
+  const { colors } =  useTheme();
+
+  return (
+    <Animated.View
+      style={{
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        backgroundColor: colors.card,
+        width: '100%',
+        height: 64,
+        transform: [{translateY: translateY}],
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        left: 0,
+        elevation: 4,
+        zIndex: 1,
+}}>
+   </Animated.View>
+  );
+};
