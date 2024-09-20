d77f4438fdd57f16b702a63d6ed74697039c0e51
Hardy Namakhwa <66388667+Hard265@users.noreply.github.com>
2024-09-20 12:12:03 +0200

    Update index.tsx

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,5 +1,18 @@
 import ProductList from "@/components/ProductList";
+import HomeAppBar from "@/components/HomeAppBar";
 
 export default function HomeScreen() {
-  return <ProductList />;
+  const scrollY = new Animated.Value(0);
+  const diffClamp = Animated.diffClamp(scrollY, 0, 64);
+  const translateY = diffClamp.interpolate({
+    inputRange: [0, 64],
+    outputRange: [0, -64],
+  });
+  
+  return <View className="flex-1">
+    <HomeAppBar translateY={translateY}/>
+    <ProductList onScroll={(e) =>{
+      scrollY.setValue(e.nativeEvent.contentOffset.y)
+    }}/>
+  </View>
 }
