df9092bc9799cb874667972830d0a44d4ea7c9a0
Hardy Namakhwa <66388667+Hard265@users.noreply.github.com>
2024-09-23 09:39:02 +0200

    Update cartActions.ts

--- src/redux/actions/cartActions.ts
+++ src/redux/actions/cartActions.ts
@@ -1,5 +1,13 @@
 import { addToCart, removeFromCart, clearCart } from "../reducers/cartReducer";
 
+export const fetch = async () => {
+  try {
+    const { data } = await axios.get('')
+  } catch {
+    console.log("error fetching cart")
+  }
+}
+
 export const addItemToCart = (item: any) => (dispatch: any) => {
   dispatch(addToCart(item));
 };
